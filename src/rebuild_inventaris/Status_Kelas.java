f076da27e27fb253c22830db64be05dd345618eb
Rangga Aditya Julian E <rangga1111@gmail.com>
2015-06-04 10:41:00 +0700

    Update Status_Kelas.java
    done

--- src/rebuild_inventaris/Status_Kelas.java
+++ src/rebuild_inventaris/Status_Kelas.java
@@ -1,114 +1,45 @@
 package rebuild_inventaris;
 
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Status_Kelas {
-
+	
+	int Jumlah;
+	String Kondisi;
+	String Posisi;
+	
 	Scanner scan = new Scanner(System.in);
-	int jumlah;
-	String kondisi;
-	String posisi;
 	
-	public void input(String masukan){
-		System.out.println("Masukkan Jumlah : ");
-		jumlah = scan.nextInt();
-		System.out.println("Masukkan Kondisi : ");
-		kondisi = scan.next();
-		System.out.println("Masukkan Posisi: ");
-		posisi = scan.next();
-	}
-	
-	public void view(String pengeluaran){
-		System.out.println("jumlah : "+jumlah);
-		System.out.println("kondisi : "+kondisi);
-		System.out.println("posisi : "+posisi);
-   }
-
-	boolean Analisis_Stop_Kontak() {
+	public void insert(){
+		System.out.println("Jumlah : ");
+		Jumlah = scan.nextInt();
 		
-		return false;
+		System.out.println("Kondisi : ");
+		Kondisi = scan.next();
+		
+		System.out.println("Posisi : ");
+		Posisi = scan.next();
 	}
-
-	String Kondisi_Stop_Kontak() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	String Posisi_Stop_Kontak() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	boolean Analisis_Lampu() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public String Analisisi_Kondisi() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String Analisisi_Posisi() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	boolean Analisis_Kipas_Angin() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	String Kondisi_Kipas_Angin() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	String Posisi_Kipas_Angin() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	boolean Analisis_Kabel_LCD() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	String Kondisi_Kabel_LCD() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	String Posisi_Kabel_LCD() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String Analisis_Kondisi() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	boolean Analisis_CCTV() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public String Posisi_AC() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean Analisis_AC() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public String Kondisi_AC() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	
+	public void view(){
+		System.out.println("Jumlah : "+Jumlah);
+		System.out.println("Kondisi : "+Kondisi);
+		System.out.println("Posisi : "+Posisi);
+	}
+	
+	public void save(){
+		try{
+			FileWriter write = new FileWriter("Status Kelas.txt");
+			write.write("Jumlah : " + Jumlah);
+			write.write("Kondisi : " + Kondisi);
+			write.write("Posisi : " + Posisi);
+			write.close();
+		}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 
 }
+
