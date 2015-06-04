f3ed5a74eeb66eb28cfd9ad2b89fde8be3cd2bd4
Rangga Aditya Julian E <rangga1111@gmail.com>
2015-06-04 10:37:49 +0700

    Update Data_Kelas.java
    saving object done

--- src/rebuild_inventaris/Data_Kelas.java
+++ src/rebuild_inventaris/Data_Kelas.java
@@ -1,33 +1,72 @@
-package rebuild_inventaris;
 
+package rebuild_inventaris;
+import java.io.FileWriter;
 import java.util.Scanner;
 
-public class Data_Kelas {
+public class Data_Kelas  {
+	static Scanner input = new Scanner(System.in);
+	static Analisis identitas = new Analisis();
 	
-	Scanner scan = new Scanner(System.in);
-	
-	int ruang_kelas;
-	String lokasi_kelas;
-	String pengguna;
-	
-		public void Data_Pengguna(){
+	void identitas(){
 		
-		System.out.println("Ruang : ");
-		ruang_kelas = scan.nextInt();
+		System.out.println("========================");
+		System.out.println("  DATA IDENTITAS RUANGAN");
+		System.out.println("========================");
+		System.out.println("Input Ruang : ");
+		identitas.setRuang(input.nextInt());
+		System.out.println("Input Lokasi : ");
+		identitas.setLokasi(input.next());
+		System.out.println("Input Fakultas : ");
+		identitas.setFakultas(input.next());
+		System.out.println("Input Jurusan : ");
+		identitas.setJurusan(input.next());
+		System.out.println("========================");
 		
-		System.out.println("Lokasi Ruang (GKB 1/GKB 2/GKB 3 : ");
-		lokasi_kelas = scan.next();
+		System.out.println("Ruang : " + identitas.getRuang());
+		System.out.println("Lokasi : " + identitas.getLokasi());
+		System.out.println("Fakultas : " + identitas.getFakultas());
+		System.out.print("Jurusan : " + identitas.getJurusan());
+	}
+	public static int Analisis_Ruang(){
+		System.out.println("\nInput Panjang Ruang : ");
+		identitas.setPanjang(input.nextInt());
+		System.out.println("Input Lebar Ruang : ");
+		identitas.setLebar(input.nextInt());
+		System.out.println("Input Jumlah Jendela : ");
+		identitas.setJum_Jendela(input.nextInt());
+		System.out.println("Input Jumlah Pintu : ");
+		identitas.setJum_Pintu(input.nextInt());
+		System.out.println("Input Jumlah Kursi : ");
+		identitas.setJum_Kursi(input.nextInt());
 		
-		System.out.println("Fakultas : ");
-		pengguna = scan.next();
-		
+		return 0;
+	}
+	public void Tampil(){
+		System.out.println("Luas : " + Luas());
+		System.out.println("Rasio : " + Rasio());
+	}
+	float Luas(){
+		return identitas.getPanjang() * identitas.getLebar();
+	}
+	double Rasio(){
+		return Luas() / identitas.getJum_Kursi();
 	}
 	
-	public void Result_Data_Pengguna(){
-		System.out.println("Ruang : "+ ruang_kelas);
-		System.out.println("Ruang : "+ lokasi_kelas);
-		System.out.println("Ruang : "+ pengguna);
-		
-	}
-	
+	public void save(){
+		try{
+			FileWriter write = new FileWriter("Data_Kelas.txt");
+			write.write("Data Kelas : " );
+			write.write(" |Ruang : " + identitas.getRuang() );
+			write.write(" |Lokasi : " + identitas.getLokasi());
+			write.write(" |Fakultas : " + identitas.getFakultas());
+			write.write(" |Jurusan : " + identitas.getJurusan());
+			write.write(" |Luas : " + Luas());
+			write.write(" |rasio: " + Rasio());
+			write.close();
+		}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+
 }
