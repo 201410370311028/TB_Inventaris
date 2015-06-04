0fa38505c235ada0ed2b74d4fada5cb2d5b7de43
Rangga Aditya Julian E <rangga1111@gmail.com>
2015-06-04 10:36:06 +0700

    Update Analisis.java
    done

--- src/rebuild_inventaris/Analisis.java
+++ src/rebuild_inventaris/Analisis.java
@@ -1,19 +1,70 @@
+
 package rebuild_inventaris;
 
-import java.util.*;
-
 public class Analisis {
-	
-	Scanner scan = new Scanner(System.in);
-	String SSID;
-	int bandwith;
-	
-	public void Analisis_WIFI(){
-		System.out.println("Masukan SSID Wifi : ");
-		SSID = scan.next();
+		private int Ruang;
+		private String Lokasi;
+		private String Fakultas;
+		private String Jurusan;
+		private int Panjang;
+		private int Lebar;
+		private int Jum_Kursi;
+		private int Jum_Pintu;
+		private int Jum_Jendela;
 		
-		System.out.println("Masukan bandwith Wifi : ");
-		bandwith = scan.nextInt();
-	}
-	
+		public String getJurusan() {
+			return Jurusan;
+		}
+		public void setJurusan(String jurusan) {
+			Jurusan = jurusan;
+		}
+		public int getRuang() {
+			return Ruang;
+		}
+		public void setRuang(int ruang) {
+			Ruang = ruang;
+		}
+		public String getLokasi() {
+			return Lokasi;
+		}
+		public void setLokasi(String lokasi) {
+			Lokasi = lokasi;
+		}
+		public String getFakultas() {
+			return Fakultas;
+		}
+		public void setFakultas(String fakultas) {
+			Fakultas = fakultas;
+		}
+		public int getPanjang() {
+			return Panjang;
+		}
+		public void setPanjang(int panjang) {
+			Panjang = panjang;
+		}
+		public int getLebar() {
+			return Lebar;
+		}
+		public void setLebar(int lebar) {
+			Lebar = lebar;
+		}
+		public int getJum_Kursi() {
+			return Jum_Kursi;
+		}
+		public void setJum_Kursi(int jum_Kursi) {
+			Jum_Kursi = jum_Kursi;
+		}
+		public int getJum_Pintu() {
+			return Jum_Pintu;
+		}
+		public void setJum_Pintu(int jum_Pintu) {
+			Jum_Pintu = jum_Pintu;
+		}
+		public int getJum_Jendela() {
+			return Jum_Jendela;
+		}
+		public void setJum_Jendela(int jum_Jendela) {
+			Jum_Jendela = jum_Jendela;
+		}
 }
+
